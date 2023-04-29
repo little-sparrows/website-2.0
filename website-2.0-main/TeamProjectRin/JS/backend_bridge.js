aa7ecaa722b0a7e2249a81bfb47c076832a33ea9
xzWhat <115475302+cofeedipso@users.noreply.github.com>
2023-04-29 07:40:04 +0300

    Add files via upload

---
+++ website-2.0-main/TeamProjectRin/JS/backend_bridge.js
@@ -0,0 +1,29 @@
+const backendDomain = "http://0.0.0.0:8000";
+
+export const getUserID = async (
+    fingerprint_id,
+    weak_fingerprint_id,
+    collected_typing_patterns,
+) => {
+
+    const baseUrl = backendDomain + '/v1/user/check';
+
+    let queryParams = '';
+    queryParams += '?fingerprint_id=' + fingerprint_id;
+    queryParams += '&weak_fingerprint_id=' + weak_fingerprint_id;
+    collected_typing_patterns.forEach(i =>
+        queryParams += '&collected_typing_patterns' + i
+    )
+
+    const endpoint = `${baseUrl}${queryParams}`;
+
+    try {
+        const response = await fetch(endpoint, {cache: 'no-cache'});
+        if (response.ok) {
+            return await response.json();
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
